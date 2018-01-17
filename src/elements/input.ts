fa3763764fefa52edbc87b3053fec71f905c8ee0
Laurence Roberts <lsjroberts@outlook.com>
2018-01-17 20:19:34 +0100

    Add form and text input elements

--- src/elements/input.ts
+++ src/elements/input.ts
@@ -1,6 +1,61 @@
 import { createElement } from "react";
+import { merge, noop } from "lodash";
 
-import { Attribute, Element } from "../internal";
+import {
+  Attribute,
+  Element,
+  Layout,
+  single,
+  transformAttrs
+} from "../internal";
+
+// -- Wrappers
+
+interface Form {
+  onSubmit: () => void;
+}
+
+export function form(
+  style: string,
+  attrs: Array<Attribute>,
+  form: Form,
+  child: Layout | Element
+): Element {
+  return createElement(
+    "form",
+    merge(transformAttrs(attrs), {
+      onSubmit: evt => {
+        evt.preventDefault();
+        form.onSubmit();
+      }
+    }),
+    child
+  );
+}
+
+// -- Textual
+
+interface Text {
+  onChange: (value: string | number) => void;
+  value: string | number | null;
+  label: Element | string;
+}
+
+export function text(
+  style: string,
+  attrs: Array<Attribute>,
+  input: Text = { onChange: noop, value: null, label: "" }
+): Element {
+  return createElement(
+    "input",
+    merge(transformAttrs(attrs), {
+      onChange: evt => input.onChange(evt.target.value),
+      value: input.value
+    })
+  );
+}
+
+// -- Controls
 
 interface Checkbox {
   onChange: (value: boolean) => void;
@@ -13,11 +68,14 @@ export function checkbox(
   attrs: Array<Attribute>,
   checkbox: Checkbox
 ): Element {
-  return createElement("label", {
-    style: {
-      display: "flex"
-    },
-    children: [
+  return createElement(
+    "label",
+    merge(transformAttrs(attrs), {
+      style: {
+        display: "flex"
+      }
+    }),
+    [
       createElement("input", {
         type: "checkbox",
         checked: checkbox.checked,
@@ -25,5 +83,29 @@ export function checkbox(
       }),
       checkbox.label
     ]
-  });
+  );
+}
+
+// -- Buttons
+
+interface Button {
+  onPress: () => void;
+  submit?: boolean;
+}
+
+export function button(
+  style: string,
+  attrs: Array<Attribute>,
+  button: Button = { onPress: noop, submit: false },
+  child: Element
+): Element {
+  return createElement(
+    "button",
+    merge(transformAttrs(attrs), {
+      onPress: evt =>
+        (<Button>button).onPress ? (<Button>button).onPress() : noop,
+      type: (<Button>button).submit ? "submit" : null
+    }),
+    child
+  );
 }
